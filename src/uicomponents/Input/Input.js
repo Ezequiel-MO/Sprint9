f92cc6e42890d7908d6a27d4c359bbe4dad248c9
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2021-10-20 13:21:25 +0200

    ADD: Basic Input Test

--- src/uicomponents/Input/Input.js
+++ src/uicomponents/Input/Input.js
@@ -1,6 +1,7 @@
 const Input = ({ type, value, name, onChange, placeholder }) => {
   return (
     <input
+      data-testid='generic-input'
       name={name}
       value={value}
       type={type}
