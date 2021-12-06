73ea27c4d92cc0d2d827aaebb9b15090e0d76d68
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2021-12-06 11:57:15 +0100

    CHNGD: Refactored HotelProjectForm into visual and logic components

--- src/pages/ProjectConfig/ProjectForms/HotelProjectForm/HotelProjectForm.js
+++ src/pages/ProjectConfig/ProjectForms/HotelProjectForm/HotelProjectForm.js
@@ -1,70 +1,22 @@
-import { useState, useEffect } from "react";
-import { useAxiosFetch } from "../../../../hooks/useAxiosFetch";
-import { useSelector } from "react-redux";
-import { selectActiveCode } from "../../../../features/ActiveCodeSlice";
-import { useHistory } from "react-router";
-import { baseAPI } from "../../../../api/axios";
 import {
   HotelProjectFormContainer,
   HotelFormContainer,
   AutoCompleteForm,
 } from "../styles";
-import useGetHotels from "../../../../hooks/useGetHotels";
 import SaveButton from "../../../../uicomponents/SaveButton/SaveButton";
-import { findSelectedOptions } from "../../utils/utils";
 import ProjectSelector from "../ProjectSelector/ProjectSelector";
 import HotelRatesTabs from "./HotelRatesTabs/HotelRatesTabs";
+import HotelPFLogic from "./HotelPFLogic";
 
 const HotelProjectForm = () => {
-  const activeCode = useSelector(selectActiveCode);
-  const history = useHistory();
-  const [formIsValid, setFormIsValid] = useState(false);
-
-  const [hotels, setHotels] = useState([]);
-
-  const { hotelOptions } = useGetHotels();
-
-  const [selectedHotelOptions, setSelectedHotelOptions] = useState([]);
-
   const {
-    data: { project: projectByCode },
-  } = useAxiosFetch(`https://cutt-events.herokuapp.com/project/${activeCode}`);
-
-  useEffect(() => {
-    if (formIsValid) {
-      if (projectByCode) {
-        try {
-          baseAPI
-            .post(`/addHotels/${projectByCode._id}`, hotels)
-            .then((res) => {
-              console.log(res);
-              setTimeout(() => history.push("/schedule-project-form"), 1000);
-            });
-        } catch (err) {
-          console.warn(err);
-        }
-      } else alert("Loading project ...");
-    }
-  }, [formIsValid]);
-
-  const storeSelectedValues = (array, action) => {
-    if (action.action === "select-option" || action.action === "remove-value") {
-      setSelectedHotelOptions(array);
-    } else if (action.action === "clear") {
-      setSelectedHotelOptions([]);
-    }
-  };
-
-  const updateInputData = () => {
-    setHotels(findSelectedOptions(selectedHotelOptions, hotelOptions));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    updateInputData();
-    /*  setFormIsValid(true); */
-  };
-
+    handleSubmit,
+    hotelOptions,
+    storeSelectedValues,
+    selectedHotelOptions,
+    projectByCode,
+    formIsValid,
+  } = HotelPFLogic();
   return (
     <>
       <HotelFormContainer>
