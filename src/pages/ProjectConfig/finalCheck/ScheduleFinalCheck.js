8ccb209c1d56ef300811c5bca2369453baa8bff6
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2021-12-28 17:43:33 +0100

    minor fixes

--- src/pages/ProjectConfig/finalCheck/ScheduleFinalCheck.js
+++ src/pages/ProjectConfig/finalCheck/ScheduleFinalCheck.js
@@ -55,6 +55,19 @@ const ScheduleFinalCheck = () => {
           <option value='transferIn'>Transfer In</option>
           <option value='transferOut'>Transfer Out</option>
         </select>
+        <select value={capacity} onChange={handleCapacityChange}>
+          <option>Vehicle Capacity</option>
+          {capacities?.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
+
+        <input
+          type='number'
+          placeholder='Add Quantity'
+          value={nrVehicles}
+          onChange={handleNrVehiclesChange}
+        />
         <button type='button' onClick={handleAddTransfer}>
           Add Transfer
         </button>
@@ -62,20 +75,6 @@ const ScheduleFinalCheck = () => {
 
       {addTransfers.transferIn && (
         <div style={{ display: "flex" }}>
-          <select value={capacity} onChange={handleCapacityChange}>
-            <option>Vehicle Capacity</option>
-            {capacities?.map((item) => (
-              <option key={item}>{item}</option>
-            ))}
-          </select>
-
-          <input
-            type='number'
-            placeholder='Add Quantity'
-            value={nrVehicles}
-            onChange={handleNrVehiclesChange}
-          />
-
           <p>
             Transfer In , {capacity} Seater Bus , cost {vendorCost}€
           </p>
@@ -84,20 +83,6 @@ const ScheduleFinalCheck = () => {
 
       {addTransfers.transferOut && (
         <div style={{ display: "flex" }}>
-          <select value={capacity} onChange={handleCapacityChange}>
-            <option>Vehicle Capacity</option>
-            {capacities?.map((item) => (
-              <option key={item}>{item}</option>
-            ))}
-          </select>
-
-          <input
-            type='number'
-            placeholder='Add Quantity'
-            value={nrVehicles}
-            onChange={handleNrVehiclesChange}
-          />
-
           <p>
             Transfer Out , {capacity} Seater Bus , cost {vendorCost}€
           </p>
