6b9880199b6845133b74984493b2321d1b971427
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2022-01-06 10:57:30 +0100

    CHNGD: Styles to ProjectLog

--- src/pages/ProjectConfig/ProjectLog/ProjectLog.js
+++ src/pages/ProjectConfig/ProjectLog/ProjectLog.js
@@ -1,14 +1,8 @@
-import {
-  ProjectLogContainer,
-  ProjectConfiguration,
-  GroupContainer,
-  ProjectResults,
-} from "./styles";
-import PLList from "./PLList/PLLists";
 import SaveButton from "../../../uicomponents/SaveButton/SaveButton";
+import "./styles.css";
 import DialogBox from "../../../uicomponents/dialogBox/DialogBox";
 import useProjectLog from "./useProjectLog";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Formik, Field, ErrorMessage, Form } from "formik";
 import * as Yup from "yup";
 
 const ProjectLog = () => {
@@ -54,47 +48,65 @@ const ProjectLog = () => {
         })}
       >
         {(formik) => (
-          <ProjectLogContainer>
-            <ProjectConfiguration>
-              <GroupContainer>
-                <legend>
-                  <h4>About the Project</h4>
-                </legend>
-                <label htmlFor='code'>Project Code</label>
-                <Field name='code' type='text' />
-                <ErrorMessage name='code' component='span' />
-                <label htmlFor='accountManager'>Account Manager</label>
-                <Field name='accountManager' type='text' />
-                <ErrorMessage name='accountManager' component='span' />
-                <label htmlFor='groupName'>Group Name</label>
-                <Field name='groupName' type='text' />
-                <ErrorMessage name='groupName' component='span' />
-                <label htmlFor='groupLocation'>Group Location</label>
-                <Field name='groupLocation' type='text' />
-                <ErrorMessage name='groupLocation' component='span' />
-                <label htmlFor='arrivalDay'>Arrival Day</label>
-                <Field name='arrivalDay' type='date' />
-                <ErrorMessage name='arrivalDay' component='span' />
-                <label htmlFor='departureDay'>Departure Day</label>
-                <Field name='departureDay' type='date' />
-                <ErrorMessage name='departureDay' component='span' />
-                <label htmlFor='nrPax'>Number of Pax</label>
-                <Field name='nrPax' type='number' />
-                <ErrorMessage name='nrPax' component='span' />
-                <label htmlFor='clientCo'>Client Company</label>
-                <Field name='clientCo' type='text' />
-                <ErrorMessage name='clientCo' component='span' />
-                <label htmlFor='clientAccManager'>Client Account Manager</label>
-                <Field name='clientAccManager' type='text' />
-                <ErrorMessage name='clientAccManager' component='span' />
-              </GroupContainer>
+          <Form className='form'>
+            <fieldset>
+              <legend>
+                <h4>About the Project</h4>
+              </legend>
+              <div className='form-inputs'>
+                <div>
+                  <label htmlFor='code'>Project Code</label>
+                  <Field name='code' type='text' />
+                  <ErrorMessage name='code' component='span' />
+                </div>
+                <div>
+                  <label htmlFor='accountManager'>Account Manager</label>
+                  <Field name='accountManager' type='text' />
+                  <ErrorMessage name='accountManager' component='span' />
+                </div>
+                <div>
+                  <label htmlFor='groupName'>Group Name</label>
+                  <Field name='groupName' type='text' />
+                  <ErrorMessage name='groupName' component='span' />
+                </div>
 
-              <ProjectResults validForm={projectFormIsValid}>
-                <PLList data={formik.values} />
-                <SaveButton type='submit' text='Save and continue' />
-              </ProjectResults>
-            </ProjectConfiguration>
-          </ProjectLogContainer>
+                <div>
+                  <label htmlFor='groupLocation'>Group Location</label>
+                  <Field name='groupLocation' type='text' />
+                  <ErrorMessage name='groupLocation' component='span' />
+                </div>
+                <div>
+                  <label htmlFor='arrivalDay'>Arrival Day</label>
+                  <Field name='arrivalDay' type='date' />
+                  <ErrorMessage name='arrivalDay' component='span' />
+                </div>
+                <div>
+                  <label htmlFor='departureDay'>Departure Day</label>
+                  <Field name='departureDay' type='date' />
+                  <ErrorMessage name='departureDay' component='span' />
+                </div>
+
+                <div>
+                  <label htmlFor='nrPax'>Number of Pax</label>
+                  <Field name='nrPax' type='number' />
+                  <ErrorMessage name='nrPax' component='span' />
+                </div>
+                <div>
+                  <label htmlFor='clientCo'>Client Company</label>
+                  <Field name='clientCo' type='text' />
+                  <ErrorMessage name='clientCo' component='span' />
+                </div>
+                <div>
+                  <label htmlFor='clientAccManager'>
+                    Client Account Manager
+                  </label>
+                  <Field name='clientAccManager' type='text' />
+                  <ErrorMessage name='clientAccManager' component='span' />
+                </div>
+              </div>
+              <SaveButton type='submit' text='Save and continue' />
+            </fieldset>
+          </Form>
         )}
       </Formik>
 
